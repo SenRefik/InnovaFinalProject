d1d2298c421056698ea39b8504119d16e21d65ce
Refik Şen <refik.sen@outlook.com>
2022-02-22 17:32:44 +0300

    Folder structure created.

--- InnovaFinalProject/SiteManagement.Application/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace SiteManagement.Application
-{
-    public class Class1
-    {
-    }
-}
---
+++ SiteManagement.Application/Contracts/Persistence/Repositories/Commons/IRepositoryBase.cs
@@ -0,0 +1,13 @@
+﻿using SiteManagement.Domain.Entities.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiteManagement.Application.Contracts.Persistence.Repositories.Commons
+{
+    public interface IRepositoryBase<T> where T : EntityBase
+    {
+    }
+}
